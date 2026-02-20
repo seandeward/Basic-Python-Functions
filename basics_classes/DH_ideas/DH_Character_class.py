78f670fe063ca6f8303a8b7ad0bf4c1b1f7c4c98
Sean DeWard <126360814+seandeward@users.noreply.github.com>
2026-02-20 16:36:09 -0500

    learning classes

---
+++ basics_classes/DH_ideas/DH_Character_class.py
@@ -0,0 +1,18 @@
+
+
+class Character:
+    def __init__(self, name,
+                max_hp, max_stress, Evasion, maj_thresh, sev_thresh, armor_score, Strength, Agility, Finesse, Knowledge, Instinct, Presence):
+        self.name = name
+        self.hp = max_hp
+        self.stress = max_stress
+        self.evasion = Evasion
+        self.major_threshold = maj_thresh
+        self.severe_threshold = sev_thresh
+        self.armor = armor_score
+        self.strength = Strength
+        self.agility = Agility
+        self.finesse = Finesse
+        self.knowledge = Knowledge
+        self.instinct = Instinct
+        self.presence = Presence
---
+++ basics_classes/public_vs_private_classes/private_vs_public.py
@@ -0,0 +1,25 @@
+
+# PUBLIC
+# by default, all properties and methods in a class are public, meaning that you can access them with the "." operator
+class Book:
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+
+# PRIVATE
+# Private data members are a way to encapsulate logic and data *within* a class definition. To make a property or method private, just prefix it with two underscores
+class Wall:
+    def __init__(self, phy_def, mag_def):
+        self.__phy_def = phy_def
+        self.__mag_def = mag_def
+        # until __name and __author are called in a method, they are grayed out, since they are now private to the class
+        
+    def get_defenses(self):
+        return self.__phy_def * self.__mag_def
+
+front_wall = Wall(10, 2)
+# results in an error
+# print(front_wall.__armor)
+
+# This works
+print(front_wall.get_defenses()) # prints 20
