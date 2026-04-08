e4e55f2bb46e9f3b1881c06c07006f9ae013249e
Sean DeWard <126360814+seandeward@users.noreply.github.com>
2026-04-08 11:01:56 -0400

    updated spacing and comments
    Standardized the spacing of the files, and updates a couple of the comments.
    
    No functional changes in this commit.

--- basics_classes/public_vs_private_classes/private_vs_public.py
+++ basics_classes/public_vs_private_classes/private_vs_public.py
@@ -1,21 +1,21 @@
 
-# PUBLIC
+#:: PUBLIC
 # by default, all properties and methods in a class are public, meaning that you can access them with the "." operator
 class Book:
-    def __init__(self, name, author):
-        self.name = name
-        self.author = author
+  def __init__(self, name, author):
+    self.name = name
+    self.author = author
 
-# PRIVATE
+#:: PRIVATE
 # Private data members are a way to encapsulate logic and data *within* a class definition. To make a property or method private, just prefix it with two underscores
 class Wall:
-    def __init__(self, phy_def, mag_def):
-        self.__phy_def = phy_def
-        self.__mag_def = mag_def
-        # until __name and __author are called in a method, they are grayed out, since they are now private to the class
-        
-    def get_defenses(self):
-        return self.__phy_def * self.__mag_def
+  def __init__(self, phy_def, mag_def):
+      self.__phy_def = phy_def
+      self.__mag_def = mag_def
+    # until __name and __author are called in a method, they are grayed out, since they are now private to the class
+
+  def get_defenses(self):
+    return self.__phy_def * self.__mag_def
 
 front_wall = Wall(10, 2)
 # results in an error
